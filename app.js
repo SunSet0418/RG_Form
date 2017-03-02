364857c373bf704ad6b9c1570a73adfb9d83b5a2
SunSet0418 <cds07016@naver.com>
2017-03-03 00:46:11 +0900

    post success

--- app.js
+++ app.js
@@ -8,4 +8,82 @@ app.use(bodyParser.urlencoded({
     extended : true
 }));
 
+mongoose.connect("mongodb://localhost/RG_New_People/", function (err) {
+    if(err){
+        console.log('DB Error!')
+        throw err
+    }
+    else {
+        console.log('DB Connect Success!')
+    }
+})
+
+var NewSchema = new schema({
+    name : {
+        type : String
+    },
+    phonenum : {
+        type : String
+    },
+    department : {
+        type : String
+    },
+    email : {
+        type : String
+    }
+})
+
+var New = mongoose.model('people', NewSchema);
+
+app.listen(8989, function (err) {
+    if(err){
+        console.log('Server Error!')
+        throw err
+    }
+    else {
+        console.log('Server Running At 8989!')
+    }
+})
+
+app.get('/', function (req, res) {
+    res.redirect('/newpeople')
+})
+
+app.post('/newpeople', function (req, res) {
+    var people = new New({
+        name : req.param('name'),
+        phonenum : req.param('phonenum'),
+        department : req.param('department'),
+        email : req.param('email')
+    })
+
+    New.findOne({
+        phonenum : req.param('phonenum')
+    }, function (err, result) {
+        if(err){
+            console.log('/newpeople Error!')
+            throw err
+        }
+        else if(result){
+            res.json({
+                success : false,
+                message : "Already In Database"
+            })
+        }
+        else {
+            people.save(function (err) {
+                if(err){
+                    console.log('save Error!')
+                    throw err
+                }
+                else {
+                    res.json({
+                        success : true,
+                        message : "Save Success"
+                    })
+                }
+            })
+        }
+    })
+})
 
