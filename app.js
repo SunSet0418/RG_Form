7d13a577a589ee1d624606cd97fcefd6c63e2a90
SunSet0418 <cds07016@naver.com>
2017-03-05 17:34:03 +0900

    asdf

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 mongoose.connect("mongodb://localhost:28001/RG_New_People/", function (err) {
     if (err) {
         console.log('DB Error!')
